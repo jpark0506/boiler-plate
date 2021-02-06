aee78e7a824d2a1e04a820c5e0d22ec28b0756e9
박준혁(JunHyeok Park) <45231740+jpark0506@users.noreply.github.com>
2021-02-06 16:38:50 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
+const bodyparser = require('body-parser');
+const { User } = require('./models/User');
+
+
+const config = require('./config/key');
+
+//node-mon 없이 코드 수정하면 서버 재접 해야함
+//node-mon 설치시 dev 붙이면 로컬에서만 사용할 수 있게 함
+
+//분석해서 가져올 수 있음
+//application/x-www-form-urlencoded
+app.use(bodyparser.urlencoded({extended:true}));
+//application/json
+app.use(bodyparser.json());
+
 
 //몽구스 서버 연결, password 부분 바꾸기
 //몽구스 서버 내에서 Security-> Network Access 부분에서 모두 사용할 수 있게 바꿔준다
-mongoose.connect('mongodb+srv://peterpark:guni0506@boiler-plate.39whi.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+//비밀 정보 보호하기
+mongoose.connect(config.mongoURI, 
 {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false})
 .then(()=>console.log('MongoDB connected')).catch(err => console.log(err))
 
@@ -13,6 +29,20 @@ mongoose.connect('mongodb+srv://peterpark:guni0506@boiler-plate.39whi.mongodb.ne
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req,res)=>{
+    //회원가입할 때 필요한 정보들을 클라이언트에서 가져오면 
+    //그것들을 데이터베이스에 넣어준다
+
+    const user = new User(req.body)
+
+    user.save((err)=>{
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success:true     
+        })
+    })
 })
 
 app.listen(port, () => {
